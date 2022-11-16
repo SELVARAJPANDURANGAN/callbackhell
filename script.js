a61a9fb684be1a55078a8c3480713a42e2d7b980
SELVARAJPANDURANGAN <116090724+SELVARAJPANDURANGAN@users.noreply.github.com>
2022-11-16 21:32:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+var multiply = function name1 (){
+    name2()
+    setTimeout(()=>{
+console.log("1");
+console.log("Happy Indipendence Day")
+},10000);
+function name2(){
+name3()
+    setTimeout(()=>{
+console.log("2");
+},9000);
+
+function name3(){
+name4()
+    setTimeout(()=>{
+console.log("3");
+},8000);
+function name4(){
+name5()
+    setTimeout(()=>{
+console.log("4");
+},7000);
+function name5(){
+name6()
+    setTimeout(()=>{
+console.log("5");
+},6000);
+function name6(){
+name7()
+    setTimeout(()=>{
+console.log("6");
+},5000);
+function name7(){
+name8()
+    setTimeout(()=>{
+console.log("7");
+},4000);
+function name8(){
+name9()
+    setTimeout(()=>{
+console.log("8");
+},3000);
+function name9(){
+name10()
+    setTimeout(()=>{
+console.log("9");
+},2000);
+function name10(){
+
+    setTimeout(()=>{
+console.log("10");
+},1000);}}}}}}}}}}
+
+multiply()
